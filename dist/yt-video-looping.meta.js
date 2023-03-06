0286a837e3efdedf0bfb6d767908c8715b2dd918
Nadelopo <mistrmorsikk@mail.ru>
2023-03-07 00:10:46 +0400

    upd

---
+++ dist/yt-video-looping.meta.js
@@ -0,0 +1,8 @@
+// ==UserScript==
+// @name        yt-video-looping
+// @version     0.0.1
+// @description yotube video looping app
+// @match       https://www.youtube.com/watch?v=rTbJ2qFd-Tc*
+// @icon        https://www.google.com/s2/favicons?sz=64&domain=youtube.com
+// @grant       GM_addStyle
+// ==/UserScript==
