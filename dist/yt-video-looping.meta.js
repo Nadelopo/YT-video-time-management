4622af3857d557b62cf1b12a90622dc8c6f0bfb6
Nadelopo <mistrmorsikk@mail.ru>
2023-03-07 02:02:04 +0400

    upd

--- dist/yt-video-looping.meta.js
+++ dist/yt-video-looping.meta.js
@@ -1,10 +1,10 @@
 // ==UserScript==
 // @name        yt-video-looping
-// @version     0.0.4
+// @version     0.0.5
 // @description yotube video looping app
 // @match       https://www.youtube.com/watch?*
 // @icon        https://www.google.com/s2/favicons?sz=64&domain=youtube.com
-// @downloadURL https://github.com/Nadelopo/YT-vide-looping/blob/main/dist/yt-video-looping.user.js
-// @updateURL   https://github.com/Nadelopo/YT-vide-looping/blob/main/dist/yt-video-looping.user.js
+// @downloadURL https://github.com/Nadelopo/YT-vide-looping/raw/main/dist/yt-video-looping.user.js
+// @updateURL   https://github.com/Nadelopo/YT-vide-looping/raw/main/dist/yt-video-looping.user.js
 // @grant       GM_addStyle
 // ==/UserScript==
--- vite.config.ts
+++ vite.config.ts
@@ -14,9 +14,9 @@ export default defineConfig((config) => {
           match: 'https://www.youtube.com/watch?*',
           icon: 'https://www.google.com/s2/favicons?sz=64&domain=youtube.com',
           downloadURL:
-            'https://github.com/Nadelopo/YT-vide-looping/blob/main/dist/yt-video-looping.user.js',
+            'https://github.com/Nadelopo/YT-vide-looping/raw/main/dist/yt-video-looping.user.js',
           updateURL:
-            'https://github.com/Nadelopo/YT-vide-looping/blob/main/dist/yt-video-looping.user.js'
+            'https://github.com/Nadelopo/YT-vide-looping/raw/main/dist/yt-video-looping.user.js'
         },
         server: {
           open: true,
