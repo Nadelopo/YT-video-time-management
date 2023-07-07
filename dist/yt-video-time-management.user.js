2b30c005471f9a08311b0e97347daae6b7854be7
Nadelopo <mistrmorsikk@mail.ru>
2023-07-08 01:37:43 +0400

    2.0.1

--- dist/yt-video-time-management.user.js
+++ dist/yt-video-time-management.user.js
@@ -1,6 +1,6 @@
 // ==UserScript==
 // @name        yt-video-time-management
-// @version     2.0.0
+// @version     2.0.1
 // @description extension for video time management
 // @match       https://www.youtube.com/watch?*
 // @icon        https://www.google.com/s2/favicons?sz=64&domain=youtube.com
