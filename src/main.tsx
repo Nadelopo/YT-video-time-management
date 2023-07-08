bf53403a757ba1a3b2f6e59f4b7a452fa026d24c
Nadelopo <mistrmorsikk@mail.ru>
2023-07-08 19:22:00 +0400

    test

--- src/main.tsx
+++ src/main.tsx
@@ -1,6 +1,9 @@
 import { render } from 'solid-js/web'
 import { App } from './components/App'
 import { waitElement } from './utils/waitForElm'
+import S from './styles.sass?inline'
+
+GM_addStyle(S)
 
 waitElement('#owner').then((parentContainer) => {
   render(() => <App />, parentContainer)
