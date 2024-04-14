c7376c279d885a6d980d3d37001b82d7aa18bc03
Nadelopo <mistrmorsikk@mail.ru>
2024-04-14 18:15:51 +0400

    upd

--- dist/yt-video-time-management.meta.js
+++
@@ -1,8 +0,0 @@
-// ==UserScript==
-// @name        yt-video-time-management
-// @version     2.0.4
-// @description extension for video time management
-// @match       https://www.youtube.com/watch?*
-// @icon        https://www.google.com/s2/favicons?sz=64&domain=youtube.com
-// @grant       GM_addStyle
-// ==/UserScript==
